005a435469bc0727bc87246a96894e21218cd033
QuyenMaiStudent <quyenmxg.124010124083@vtc.edu.vn>
2026-07-24 17:52:44 +0700

    fix: use relative API URL for Nginx proxy

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,6 @@
 import type { Note, CreateNoteData, UpdateNoteData } from '../types/note'
 
-const BASE_URL = 'http://localhost:8080/api'
+const BASE_URL = ''
 
 class ApiError extends Error {
   status: number
